e8d7c53e97d2e5e97619d12324201b130965f159
junyong.park <junyong.park@illuminarean.com>
2020-07-13 15:17:33 +0900

    fix: Dockerfile 추가

--- README.md
+++
@@ -1,4 +0,0 @@
-This example shows how Echoswagger create a Swagger UI document page automatically.
-
-The doc contents in this example is very similar to the default example in http://editor.swagger.io/
-
--- main.go
+++ main.go
@@ -5,6 +5,11 @@ import (
 	"github.com/pangpanglabs/echoswagger/v2"
 )
 
+var (
+	Version string
+	Commit  string
+)
+
 func main() {
 	e := initServer().Echo()
 	e.Logger.Fatal(e.Start(":1323"))
@@ -14,13 +19,9 @@ func initServer() echoswagger.ApiRoot {
 	e := echo.New()
 
 	se := echoswagger.New(e, "doc/", &echoswagger.Info{
-		Title:          "Swagger Petstore",
-		Description:    "This is a sample server Petstore server.  You can find out more about     Swagger at [http://swagger.io](http://swagger.io) or on [irc.freenode.net, #swagger](http://swagger.io/irc/).      For this sample, you can use the api key `special-key` to test the authorization     filters.",
-		Version:        "1.0.0",
-		TermsOfService: "http://swagger.io/terms/",
-		Contact: &echoswagger.Contact{
-			Email: "apiteam@swagger.io",
-		},
+		Title:       "Rest Api Version:" + Version + Commit,
+		Description: "This is a sample server Petstore server.  You can find out more about     Swagger at [http://swagger.io](http://swagger.io) or on [irc.freenode.net, #swagger](http://swagger.io/irc/).      For this sample, you can use the api key `special-key` to test the authorization     filters.",
+		Version:     Version + Commit,
 		License: &echoswagger.License{
 			Name: "Apache 2.0",
 			URL:  "http://www.apache.org/licenses/LICENSE-2.0.html",
@@ -36,7 +37,7 @@ func initServer() echoswagger.ApiRoot {
 
 	se.SetExternalDocs("Find out more about Swagger", "http://swagger.io").
 		SetResponseContentType("application/xml", "application/json").
-		SetUI(echoswagger.UISetting{DetachSpec: true, HideTop: true}).
+		SetUI(echoswagger.UISetting{DetachSpec: true, HideTop: false}).
 		SetScheme("http")
 
 	PetController{}.Init(se.Group("pet", "/pet"))
